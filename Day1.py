4c05aaa6999c4ad2b4f36124eb75bc8d41489147
poojitha0306 <poojitha.v.duggappa@gmail.com>
2020-07-27 20:36:46 -0400

    My first problem from Advent

--- Day1.py
+++ Day1.py
@@ -1,2 +1,9 @@
+import math
 def CalculateMass():
-    
+    fuel = 0
+    mass = open('Day1_input.txt','r')
+    for i in mass:
+        fuel = fuel + ((math.floor(int(i)/3))-2)
+    print (fuel)
+
+CalculateMass()
